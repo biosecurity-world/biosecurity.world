9d9d120546e13f2fc803924dd86658ab9011a693
Félix Dorn <git@felixdorn.fr>
2024-09-01 03:56:20 +0200

    remove hard-coded sidebar's width

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     @vite('resources/css/app.css')
 </head>
 <body class="h-full antialiased w-full">
-<header class="w-full pb-8  pt-6">
+<header class="w-full pb-8  pt-6 px-8 xl:px-0">
     <div class="max-w-7xl mx-auto">
         <nav>
             <ul class="flex justify-between">
@@ -50,54 +50,52 @@
     </div>
 </header>
 
-<div class="w-full h-full px-36 rounded-3xl">
-    <div class="flex h-full">
-        <aside class="relative">
-            <div class="w-full h-full max-w-lg border-r divide-y border-y border-l rounded-l-3xl flex flex-col">
-                <section class="p-6 bg-white rounded-l-3xl">
-                    <h3 class="text-xl font-display flex-grow">
-                        Explore the map.
-                    </h3>
+<div class="w-full h-full xl:px-36 flex">
+    <aside class="w-full h-full border-r divide-y border-y border-l max-w-md rounded-l-3xl flex flex-col">
+        <section class="p-6 bg-white rounded-l-3xl">
+            <h3 class="text-xl font-display flex-grow">
+                Explore the map.
+            </h3>
 
-                    <p class="text-gray-700 mt-1">
-                        Last updated on
-                        <time datetime="{{ $lastEditedAt->toIso8601String() }}"
-                              title="{{ $lastEditedAt->toIso8601String() }}">
-                            {{ $lastEditedAt->format('F j, Y') }}.
-                        </time>
-                    </p>
+            <p class="text-gray-700 mt-1">
+                Last updated on
+                <time datetime="{{ $lastEditedAt->toIso8601String() }}"
+                      title="{{ $lastEditedAt->toIso8601String() }}">
+                    {{ $lastEditedAt->format('F j, Y') }}.
+                </time>
+            </p>
 
-                    <div class="relative mt-2">
-                        <input type="text" autocomplete="off"
-                               class="bg-white border-gray-200 rounded-xl py-2.5 pr-11 pl-4 w-full shadow-sm border focus:outline-none focus:ring transition focus:ring-emerald-600"
-                               placeholder="Search '1Day Sooner' or 'Research labs'">
-                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
-                            <x-heroicon-s-magnifying-glass class="size-5 text-gray-500" />
-                        </div>
-                    </div>
+            <div class="relative mt-2">
+                <input type="text" autocomplete="off"
+                       class="bg-white border-gray-200 rounded-xl py-2.5 pr-11 pl-4 w-full shadow-sm border focus:outline-none focus:ring transition focus:ring-emerald-600"
+                       placeholder="Search '1Day Sooner' or 'Research labs'">
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
+                    <x-heroicon-s-magnifying-glass class="size-5 text-gray-500" />
+                </div>
+            </div>
 
-                    <div class="mt-4 flex items-center justify-between">
-                                  <span class="flex flex-grow flex-col">
-                                    <span class="text-sm font-medium leading-6 text-gray-900" id="availability-label">
-                                        Highlight recently added entries
-                                    </span>
-                                    <span class="text-sm text-gray-500" id="availability-description">
-                                        Highlight entries added in the last month.
-                                    </span>
-                                  </span>
-                        <label for="recent" data-toggle="off"
-                               class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:ring-offset-2"
-                               role="switch" aria-checked="false" aria-labelledby="availability-label"
-                               aria-describedby="availability-description">
-                            <span aria-hidden="true"
-                                  class="pointer-events-none inline-block h-5 w-5 translate-x-0 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"></span>
-                            <input type="checkbox" class="sr-only" aria-hidden="true"
-                                   aria-labelledby="availability-label" aria-describedby="availability-description"
-                                   id="recent" name="recent">
-                        </label>
-                    </div>
-                </section>
-                <section class="p-6 bg-gray-50 flex-grow rounded-bl-3xl">
+            <div class="mt-4 flex items-center justify-between">
+                      <span class="flex flex-grow flex-col">
+                        <span class="text-sm font-medium leading-6 text-gray-900" id="availability-label">
+                            Highlight recently added entries
+                        </span>
+                        <span class="text-sm text-gray-500" id="availability-description">
+                            Highlight entries added in the last month.
+                        </span>
+                      </span>
+                <label for="recent" data-toggle="off"
+                       class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:ring-offset-2"
+                       role="switch" aria-checked="false" aria-labelledby="availability-label"
+                       aria-describedby="availability-description">
+                <span aria-hidden="true"
+                      class="pointer-events-none inline-block h-5 w-5 translate-x-0 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"></span>
+                    <input type="checkbox" class="sr-only" aria-hidden="true"
+                           aria-labelledby="availability-label" aria-describedby="availability-description"
+                           id="recent" name="recent">
+                </label>
+            </div>
+        </section>
+        <section class="p-6 bg-gray-50 flex-grow rounded-bl-3xl">
                     <h4 class="text-lg font-display">Filters</h4>
 
                     <div class="mt-2">
@@ -159,8 +157,8 @@
                                         @endif
 
                                         <span class="ml-1 leading-none group-hover:opacity-75">
-                                                {{ Str::limit($activity->label, 35) }}
-                                            </span>
+                                            {{ Str::limit($activity->label, 35) }}
+                                        </span>
 
                                         <input type="checkbox" checked name="activities"
                                                id="activity_{{ IdHash::hash($activity->id) }}"
@@ -171,28 +169,9 @@
                         </ul>
                     </div>
                 </section>
-            </div>
+    </aside>
 
-            <div class="absolute left-[calc(26rem+1px)] top-0 z-20 w-full h-full max-w-md border-y"
-                 id="entry-aside"></div>
-            <div
-                class="absolute left-[calc(26rem+1px)] top-0 z-20 w-full h-full max-w-md border-y pointer-events-none flex justify-center border-r bg-gray-50 rounded-r-3xl pt-16 htmx-indicator"
-                id="entry-loader">
-                <svg class="inline h-8 w-8 animate-spin text-gray-200 fill-emerald-600"
-                     viewBox="0 0 100 101" fill="none"
-                     xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"></path>
-                    <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"></path>
-                </svg>
-                <span class="sr-only">Loading...</span>
-            </div>
-        </aside>
-
-        <main class="w-full h-full relative border-y border-r rounded-r-3xl">
+    <main class="w-full h-full relative border-y border-r rounded-r-3xl">
             <section data-state="error" aria-hidden="true"
                      class="app-state state-inactive">
                 <div class="text-center">
@@ -234,17 +213,21 @@
                 </div>
             </section>
             <section data-state="success" class="app-state state-inactive flex" aria-hidden="true">
-                <div class="absolute bottom-6 right-6">
-                    <div class="flex flex-col bg-white shadow divide-y rounded-lg">
-                        <button class="p-2 hover:bg-gray-50 rounded-t-lg" onclick="zoomIn()">
-                            <x-heroicon-s-plus class="size-5 text-gray-700" />
-                        </button>
-
-                        <button class="p-2 hover:bg-gray-50 rounded-b-lg" onclick="zoomOut()">
-                            <x-heroicon-s-minus class="size-5 text-gray-700" />
-                        </button>
-                    </div>
+                <div class="absolute inset-0 z-20 w-full h-full max-w-md border-y" id="entry-aside"></div>
+                <div class="absolute inset-0 z-20 w-full h-full max-w-md border-y pointer-events-none flex justify-center border-r bg-gray-50 rounded-r-3xl pt-16 htmx-indicator" id="entry-loader">
+                    <svg class="inline h-8 w-8 animate-spin text-gray-200 fill-emerald-600"
+                         viewBox="0 0 100 101" fill="none"
+                         xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"></path>
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"></path>
+                    </svg>
+                    <span class="sr-only">Loading...</span>
                 </div>
+
                 <svg role="main" id="map" width="100%" height="100%">
                     <g id="zoom-wrapper">
                         <g id="center-wrapper">
@@ -281,12 +264,23 @@
                         </g>
                     </g>
                 </svg>
+                <div class="absolute bottom-6 right-6">
+                    <div class="flex flex-col bg-white shadow divide-y rounded-lg">
+                        <button class="p-2 hover:bg-gray-50 rounded-t-lg" onclick="zoomIn()">
+                            <x-heroicon-s-plus class="size-5 text-gray-700" />
+                        </button>
+
+                        <button class="p-2 hover:bg-gray-50 rounded-b-lg" onclick="zoomOut()">
+                            <x-heroicon-s-minus class="size-5 text-gray-700" />
+                        </button>
+                    </div>
+                </div>
             </section>
-        </main>
-    </div>
+    </main>
+</div>
 
-
-    <footer aria-labelledby="footer-heading" class="mt-12 border-t border-gray-900/10 py-4">
+<footer aria-labelledby="footer-heading" class="px-4 xl:px-36">
+    <div class="mt-12 border-t border-gray-900/10 py-4 ">
         <h2 id="footer-heading" class="sr-only">Footer</h2>
         <div class="flex justify-between">
             <p>
@@ -327,8 +321,8 @@
                 </li>
             </ul>
         </div>
-    </footer>
-</div>
+    </div>
+</footer>
 
 {{-- Necessary for spatie/laravel-export to find the entries, adding /_/entries to config('export.paths') doesn't crawl the URL on it. --}}
 <a href="/_/entries" class="hidden" aria-hidden="true">entries</a>
