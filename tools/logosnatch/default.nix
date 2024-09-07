0c6b491ff00bd22718a74d8a19d6eb49f62fbf1f
Félix Dorn <github@felixdorn.fr>
2024-09-07 15:07:38 +0200

    switch to gomod2nix

---
+++ tools/logosnatch/default.nix
@@ -0,0 +1,24 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+, buildGoApplication ? pkgs.buildGoApplication
+}:
+
+buildGoApplication {
+  pname = "logosnatch";
+  version = "0.1";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+  buildInputs = [
+    pkgs.libwebp
+  ];
+}
