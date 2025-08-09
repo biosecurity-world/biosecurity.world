58787a004d76bd593821f9a349466230f68288a7
Félix Dorn <git@xfe.li>
2025-08-09 19:19:14 +0200

    Update deployment workflow to use Nix and Chrome

--- flake.nix
+++ flake.nix
@@ -31,7 +31,6 @@
           go
           wrangler
           chromium
-          google-chrome
         ]);
     };
   };
--- resources/views/how-to-contribute.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layouts.default title="How to contribute?" class="bg-gray-100 antialiased">
-    <x-navbar border class="mt-4 lg:mt-8 lg:bg-white" />
-    <main class="mx-auto mt-8 max-w-7xl px-6 xl:px-0">
-        <p class="font-display text-primary-900 max-w-3xl text-4xl font-bold tracking-tight sm:text-6xl">
-            How to contribute?
-        </p>
-    </main>
-    <x-footer />
-</x-layouts.default>
